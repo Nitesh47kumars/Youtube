f42f7212445a518d33e0aa7e6f12a9b3d697c7cb
Nitesh47kumars <niteshshah028@gmail.com>
2026-01-14 18:24:47 +0530

    Add GetHistory Endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -366,28 +367,75 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       },
     },
     {
-      $project:{
-        fullName:1,
-        userName:1,
-        subscribersCount:1,
-        channelsSubscribedToCount:1,
-        avatar:1,
-        email:1,
-        coverImage:1,
-      }
-    }
+      $project: {
+        fullName: 1,
+        userName: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        avatar: 1,
+        email: 1,
+        coverImage: 1,
+      },
+    },
   ]);
 
-  if(!channel?.length){
-    throw new ApiError(404,"Channel does not exists!");
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exists!");
   }
 
   return res
-  .status(200)
-  .json(
-    new ApiResponse(200,channel[0],"Channel Fetched Successfully.")
-  )
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "Channel Fetched Successfully."));
+});
 
+const getWactchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    userName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields:{
+              owner:{
+                $first: "$owner"
+              }
+            }
+          }
+        ],
+      },
+    },
+  ]);
+
+  console.log("WatchHistory:",user);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user[0].wactchHistory,"watch History Fetch Successfully!"))
 });
 
 export {
@@ -400,5 +448,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWactchHistory
 };
